fa8a533585aaa814bb983293804e3669e37b9ad5
Rebecca Meritz <rebecca@meritz.com>
2013-05-05 15:59:41 +0200

    Done in under an hour

--- src/hq9+.erl
+++ src/hq9+.erl
@@ -12,5 +12,23 @@ run(Program) ->
 invalid_chars(Program) -> 
   lists:filter(fun(Char) -> not lists:member(Char, "HQ9+") end, Program).
 
-eval(_Program) ->
-  erlang:display("Eval").
+eval(Program) ->
+  lists:foldr(fun($H, Acc) -> hello_world(), Acc;
+                 ($Q, Acc) -> quine(Program), Acc;
+                 ($9, Acc) -> beer(), Acc;
+                 ($+, Acc) -> Acc + 1 end, 0, Program).
+
+hello_world() ->
+  erlang:display("Hello World").
+
+quine(Program) ->
+  erlang:display(Program).
+
+beer() ->
+  beer(99).
+
+beer(1) ->
+  erlang:display("1 bottle of beer on the wall, 1 bottle of beer, take 1 down pass it around, no bottles of beer on the wall");
+beer(N) ->
+  io:format("~p bottles of beer on the wall ~p bottles of beer, take 1 down pass it around, ~p bottles of beer on the wall", [N, N, N-1]),
+  beer(N-1).
